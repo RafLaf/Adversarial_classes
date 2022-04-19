7f49d5acd39a899e5ce88be8cd649a109c2801e8
RafLaf <ralafarg@gmail.com>
2022-04-19 14:08:39 +0200

    update couples

--- test64.sh
+++ test64.sh
@@ -7,8 +7,8 @@ python main.py --dataset-path /users/local/datasets --dataset miniimagenet --mod
 
 
 
-python main.py --dataset-path /users/local/datasets --dataset miniimagenet --model resnet12 --epochs 0   --milestones 100 --batch-size 128 --preprocessing ME  --test-features ../features/full1  --wandb brain-imt --forced-class --n-ways 2
+python main.py --dataset-path /users/local/datasets --dataset miniimagenet --model resnet12 --epochs 0   --milestones 100 --batch-size 128 --preprocessing ME  --test-features ../features/full1  --wandb brain-imt --force-couples --n-ways 2
 
 
 
-python main.py --dataset-path /users/local/datasets --dataset miniimagenet --model resnet12 --epochs 0   --milestones 100 --batch-size 128 --preprocessing ME  --test-features ../features/f11  --wandb brain-imt  --nb-of-rm 1 --forced-class --n-ways 2
+python main.py --dataset-path /users/local/datasets --dataset miniimagenet --model resnet12 --epochs 0   --milestones 100 --batch-size 128 --preprocessing ME  --test-features ../features/f11  --wandb brain-imt  --nb-of-rm 1 --force-couples --n-ways 2
